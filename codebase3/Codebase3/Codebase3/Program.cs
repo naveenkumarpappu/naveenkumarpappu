06f506984f266552b84e683a0be67ba7cceb6b5e
naveenkumarpappu <naveenpappu808@gmail.com>
2022-06-01 16:28:29 +0530

    c#codebase3

---
+++ codebase3/Codebase3/Codebase3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace Codebase3
+{
+    public class Employee
+
+    {
+
+        public static void Main()
+
+        {
+            string connectionString = "Data Source=LAPTOP-JOMHCDDM\SQLEXPRESS01;" + "Initial Catalog=store;Integrated Security=SSPI";
+            string procedure = "CustomerAdd";
+            String strConnString = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+            SqlConnection con = new SqlConnection(strConnString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "GetEmployeeDetailsByID";
+            cmd.Parameters.Add("@EmployeeID", SqlDbType.Int).Value = txtID.Text.Trim();
+            cmd.Connection = con;
+            try
+            {
+                con.Open();
+                GridView1.EmptyDataText = "No Records Found";
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+        }
+    }
+}
